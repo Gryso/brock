fd4e3214d45414a94a82d7cee08de76b7c4db117
gryso <richard@whys.dev>
2021-01-22 14:29:14 +0100

    feat(layout): create a basic layout and dark theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "./theme";
+import {AppBar, makeStyles, Paper, Toolbar, Typography} from "@material-ui/core";
+import {ThemeProvider} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    position: "relative",
+  },
+  main: {
+    width: "auto",
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: 600,
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    padding: theme.spacing(1),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      marginTop: theme.spacing(4),
+      marginBottom: theme.spacing(4),
+      padding: theme.spacing(3),
+    },
+  },
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <CssBaseline />
+  const classes = useStyles();
 
-    </div>
+  return (
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="absolute" color="default" className={classes.header}>
+          <Toolbar>
+            <Typography variant="h6" noWrap>
+              Brock
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <main className={classes.main}>
+          <Paper className={classes.paper}>
+            <Typography component="h1" variant="h4" align="center">
+              Pokomenos
+            </Typography>
+
+          </Paper>
+        </main>
+    </ThemeProvider>
   );
 }
 

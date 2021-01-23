07ba355d1e17304f3b2e7ec2fcf3472b27a1d6fc
gryso <richard@whys.dev>
2021-01-23 15:23:10 +0100

    fix(pokemon): rework to use MUI Accordion
    * add MUI/icons

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,7 +1,13 @@
-import {Box, Button, ListItem, ListItemSecondaryAction, ListItemText, makeStyles} from "@material-ui/core";
+import {Accordion, AccordionDetails, AccordionSummary, ListItem, makeStyles, Typography} from "@material-ui/core";
 import React from "react";
+import axios from "axios";
+import PokemonDetails from "./PokemonDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles(() => ({
+  root: {
+    width: "100%"
+  },
   name: {
     textTransform: "capitalize",
     fontWeight: "bold"
@@ -11,23 +17,41 @@ const useStyles = makeStyles(() => ({
 function Pokemon({name, url}) {
   const classes = useStyles();
 
+  const [details, setDetails] = React.useState(null);
+
+  function fetchDetails() {
+    axios.get(url)
+      .then(response => {
+        setDetails(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
   return (
-    <ListItem>
-      <ListItemText>
-        <Box className={classes.name}>
-          {name}
-        </Box>
-      </ListItemText>
-      <ListItemSecondaryAction>
-        <Button
-          size="small"
-          onClick={() => {
-            console.log('%c name:', 'color: rgb(49, 193, 27)', name);
-            console.log('%c url:', 'color: rgb(49, 193, 27)', url);
-          }}>
-          Show details
-        </Button>
-      </ListItemSecondaryAction>
+    <ListItem dense>
+      <Accordion
+        className={classes.root}
+        TransitionProps={{
+          mountOnEnter: true
+        }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          IconButtonProps={{
+            onClick: () => {
+              if (!details) {
+                fetchDetails();
+              }
+            }
+          }}
+        >
+          <Typography className={classes.name}>{name}</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          {<PokemonDetails details={details} />}
+        </AccordionDetails>
+      </Accordion>
     </ListItem>
   );
 }

00906dd2880abda94df16eb79aa2bf1576a27463
gryso <richard@whys.dev>
2021-01-23 18:08:19 +0100

    fix: remove StrictMode to fix findDOMNode error
    * MUI Accordion uses findDOMNode to pass
    Transition
    * fix missing semicolon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-import "./index.css"
+import "./index.css";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById("root")
 );
